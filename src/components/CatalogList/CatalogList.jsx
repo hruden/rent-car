1b865554653b9dea045f29d408a87a53a8826a3a
hruden <119438895+hruden@users.noreply.github.com>
2023-12-07 13:51:11 +0200

    create catalogList

--- src/components/CatalogList/CatalogList.jsx
+++ src/components/CatalogList/CatalogList.jsx
@@ -1,5 +1,30 @@
-export default function CatalogList ({list}) {
-    return (
-        <div>CatalogList</div>
-    )
+export default function CatalogList({ list }) {
+  return (<>
+      {list?.map(({make,model,year,img,rentalPrice,address,rentalCompany,type,mileage,functionalities,id})=>(
+        <div key={id}>
+        <img src={img} alt="car" width={274} height={268}/>
+        <ul>
+          <div>
+            <li>{make}</li>
+            <li>{year}</li>
+            <li>{rentalPrice}</li>
+          </div>
+          <div>
+            <div>
+              <li>{address}</li>
+              <li>{rentalCompany}</li>
+            </div>
+            <div>
+              <li>{type}</li>
+              <li>{model}</li>
+              <li>{mileage}</li>
+              <li>{functionalities[0]}</li>
+            </div>
+          </div>
+        </ul>
+      </div>  
+    ))
+}
+</>
+  );
 }
