86ed457daaf0c9edd8c09c54d930d9b7dd765d13
hruden <119438895+hruden@users.noreply.github.com>
2023-12-13 12:33:15 +0200

    fix styled

--- src/components/CatalogItem/CatalogItem.styled.js
+++ src/components/CatalogItem/CatalogItem.styled.js
@@ -1,82 +1,86 @@
 import styled from 'styled-components';
-import { FaRegHeart } from "react-icons/fa";
-import { FaHeart } from "react-icons/fa";
-
+import { FaRegHeart } from 'react-icons/fa';
+import { FaHeart } from 'react-icons/fa';
 
 export const Card = styled.div`
-position: relative;
-width: 280px;
-margin: 20px auto 15px;
-scale: 1;
-transition: scale var(--transition);
-box-shadow: 3px 8px 14px 0px #88c6fd30;
-&:hover,:focus{
-  scale: 1.05;
-}
+  position: relative;
+  width: 280px;
+  margin: 20px auto 15px;
+  scale: 1;
+  transition: scale var(--transition);
+  box-shadow: 3px 8px 14px 0px #88c6fd30;
+  &:hover,
+  :focus {
+    scale: 1.05;
+  }
 
-@media screen and (min-width: 768px) {
-  margin: 0;
-  margin-top: 20px;
-}
-`
+  @media screen and (min-width: 768px) {
+    margin: 0;
+    margin-top: 20px;
+  }
+`;
 
 export const FavoriteIconCheck = styled(FaHeart)`
-position: absolute;
-top: 14px;
-right: 14px;
-width: 18px;
-height: 18px;
-cursor: pointer;
-fill: var(--brand);
-`
+  position: absolute;
+  top: 14px;
+  right: 14px;
+  width: 18px;
+  height: 18px;
+  cursor: pointer;
+  fill: var(--brand);
+`;
 export const FavoriteIcon = styled(FaRegHeart)`
-position: absolute;
-top: 14px;
-right: 14px;
-width: 18px;
-height: 18px;
-cursor: pointer;
-fill: var(--white);
-`
+  position: absolute;
+  top: 14px;
+  right: 14px;
+  width: 18px;
+  height: 18px;
+  cursor: pointer;
+  fill: var(--white);
+`;
 export const ContainerImg = styled.div`
-width: 280px;
-height: 210px;
-`
+  width: 100%;
+  height: 268px;
+  overflow: hidden;
+`;
 export const Img = styled.img`
-border-radius: 14px;
-border: transparent;
-`
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+  border-radius: 14px;
+  border: transparent;
+`;
 export const List = styled.ul`
-padding: 0 5px;
-margin: 14px 0 28px;
-`
+  padding: 0 5px;
+  margin: 14px 0 28px;
+`;
 export const TitleContainer = styled.div`
-display: flex;
-justify-content: space-between;
-font-weight: 500;
-color: var(--textTitle);
-margin-bottom: 8px;
-`
+  display: flex;
+  justify-content: space-between;
+  font-weight: 500;
+  color: var(--textTitle);
+  margin-bottom: 8px;
+`;
 export const TextContainer = styled.div`
-color: var(--textInfo);
-`
+  color: var(--textInfo);
+`;
 export const Item = styled.li`
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
-`
+`;
 export const BtnLearnMore = styled.button`
-padding: 12px;
-width: 100%;
-font-weight: 600;
-border: transparent;
-border-radius: 12px;
-cursor: pointer;
-color: var(--white);
-background-color: var(--brand);
-transition: background-color var(--transition);
-&:hover,:focus{
-  background-color: var(--accent);
-
-}
-`
+  padding: 12px;
+  width: 100%;
+  font-weight: 600;
+  border: transparent;
+  border-radius: 12px;
+  cursor: pointer;
+  color: var(--white);
+  background-color: var(--brand);
+  transition: background-color var(--transition);
+  &:hover,
+  :focus {
+    background-color: var(--accent);
+  }
+`;
