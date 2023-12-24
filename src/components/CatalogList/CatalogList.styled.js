ede6966f6f1e67dde4b10c854c1de2a85239492d
hruden <119438895+hruden@users.noreply.github.com>
2023-12-24 19:10:44 +0200

    update styled

--- src/components/CatalogList/CatalogList.styled.js
+++ src/components/CatalogList/CatalogList.styled.js
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 
 export const ContainerList = styled.div`
   @media screen and (min-width: 768px) {
-display:flex;
-flex-wrap: wrap;
-gap: 30px;
-justify-content: start;
-}
-`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 30px;
+    justify-content: start;
+  }
+`;
